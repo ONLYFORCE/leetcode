ad52286cef3fd8a5ba615ef0d1d8a22ed5feb1ce
ONLYFORCE <527948812@163.com>
2021-05-07 19:10:45 +0800

    add BaseBasicVar.java

---
+++ src/main/java/BaseBasicVar.java
@@ -0,0 +1,37 @@
+import java.util.Collection;
+import java.util.Collections;
+
+public class BaseBasicVar {
+
+    // primitive type
+    private boolean varPriBoolean;
+    private byte varPriByte;
+    private short varPriShort;
+    private int varPriInt;
+    private float varPriFloat;
+    private long varPriLong;
+    private double varPriDouble;
+    private char varPriChar;
+
+    // reference type
+    private  Boolean varRefBoolean;
+    private  Byte varRefByte;
+    private Short varRefShort;
+    private Integer varRefInt;
+    private Float varRefFloat;
+    private Long varRefLong;
+    private Double varRefDouble;
+    private Character varRefChar;
+
+
+
+
+
+    public static  void  main(String[] args){
+
+
+
+
+
+    }
+}
